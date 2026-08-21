90e76483880e5dcd30de4fb837b4de063d1ea379
dogshitass <breixylled@gmail.com>
2026-08-21 21:32:30 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,24 +1,22 @@
+local key = script_key
 local HttpService = game:GetService("HttpService")
 local user = game.Players.LocalPlayer.Name
 
-local function check()
-    local req = syn.request or request or http_request
-    local res = req({
-        Url = "http://localhost:3000/redeem",
-        Method = "POST",
-        Headers = {["Content-Type"] = "application/json"},
-        Body = HttpService:JSONEncode({key = script_key, hwid = "N/A", username = user})
-    })
-    
-    if res then
-        return HttpService:JSONDecode(res.Body)
+local req = syn.request or request or http_request
+local res = req({
+    Url = "https://whitelistbackend.onrender.com/redeem",
+    Method = "POST",
+    Headers = {["Content-Type"] = "application/json"},
+    Body = HttpService:JSONEncode({key = key, hwid = "N/A", username = user})
+})
+
+if res and res.StatusCode == 200 then
+    local data = HttpService:JSONDecode(res.Body)
+    if data.status == "success" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/assthruster3000/DogshitFling/refs/heads/main/DogshitCarFling"))()
+    else
+        game.Players.LocalPlayer:Kick(data.message or "Invalid Key")
     end
-end
-
-local data = check()
-
-if data and data.status == "success" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/assthruster3000/DogshitFling/refs/heads/main/DogshitCarFling"))()
 else
-    game.Players.LocalPlayer:Kick(data and data.message or "Invalid Key")
+    warn("Server is down or asleep!")
 end
